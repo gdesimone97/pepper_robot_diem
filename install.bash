3e877011f698729ad179438ba2aa5e5f4e8205e3
Giuseppe De Simone <44608428+peppo97@users.noreply.github.com>
2022-09-08 16:02:44 +0200

    Update install.bash

--- install.bash
+++ install.bash
@@ -5,6 +5,9 @@ sudo apt install -y python3-rosdep python3-pip python3-catkin-tools git
 WS=$HOME/catkin_ws/src
 mkdir -p $WS
 cd $WS
+wget https://github.com/aldebaran/libqi-python/releases/download/qi-python-v3.0.0/qi-3.0.0-cp38-cp38-linux_x86_64.whl
+python3 -m pip install ./qi*
+rm qi-3.0.0*
 git clone https://github.com/peppo97/cogrob_pepper_nodes.git 
 sudo rosdep init
 rosdep update
