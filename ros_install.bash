2e495f275d92bef8071646ec2d2634c76e96692e
Giuseppe De Simone <44608428+gdesimone97@users.noreply.github.com>
2023-10-18 09:11:02 +0200

    Create ros_install.bash

---
+++ ros_install.bash
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+sudo apt update
+sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
+sudo apt install -y curl # if you haven't already installed curl
+curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add
+sudo apt update
+sudo apt install -y ros-noetic-desktop-full
+echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
+source ~/.bashrc
+sudo apt install -y python3-rosdep python3-rosinstall python3-rosinstall-generator python3-wstool build-essential
+sudo apt install -y python3-rosdep
+sudo rosdep init
+rosdep update
